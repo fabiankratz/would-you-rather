1eeef27216d973ddceba0c4508ff16455327c3ec
fabiankratz <30731536+fabiankratz@users.noreply.github.com>
2020-06-18 13:49:40 +0200

    add hook up listeners on question answer view

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React , {useState } from 'react'
 import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import {handleAnswerQuestion} from '../actions/questions'
 
 export const Question = (props) => {
   const { qid } = useParams()
-  const {questions, users, authedUser} = props
+  const {questions, users, authedUser, answerQuestion} = props
   const question = questions[qid]
+  const answer = users[authedUser].answers[qid]
+  const [selectedOption, setSelectedOption] = useState(answer || null)
   if (question) {
+    const handleAnswerQuestion = (e) => {
+      e.preventDefault()
+      answerQuestion({qid, answer: selectedOption})
+    }
     const opt1Votes = question.optionOne.votes.length
     const opt2Votes = question.optionTwo.votes.length
     const totalVotes = opt1Votes + opt2Votes
-    const answer = users[authedUser].answers[qid]
     return (
       <div>
         <img src={users[question.author].avatarURL} alt={`avatar for ${users[question.author].name}`} width="50" height="50"/>
         <h1>Would you rather...</h1>
-        <form>
-          <input type="radio" id="optionOneRadio" name="option" disabled={answer ? true : false}/>
+        <form onSubmit={(e) => handleAnswerQuestion(e)}>
+          <input 
+            type="radio" 
+            id="optionOneRadio" 
+            name="option" 
+            disabled={answer ? true : false} 
+            value="optionOne" 
+            onChange={(e) => setSelectedOption(e.target.value)}
+            checked={selectedOption === "optionOne"}
+          />
           <label htmlFor="optionOneRadio">{question.optionOne.text} ? {answer && `|| ${(opt1Votes/totalVotes)*100}% (${opt1Votes})`}</label>
-          <input type="radio" id="optionTwoRadio" name="option" disabled={answer ? true : false}/>
+          <input 
+            type="radio" 
+            id="optionTwoRadio" 
+            name="option" 
+            disabled={answer ? true : false} 
+            value="optionTwo" 
+            onChange={(e) => setSelectedOption(e.target.value)}
+            checked={selectedOption === "optionTwo"}
+          />
           <label htmlFor="optionTwoRadio">{question.optionTwo.text} ? {answer && `|| ${(opt2Votes/totalVotes)*100}% (${opt2Votes})`}</label>
           <input type="submit" value="Submit" disabled={answer ? true : false}/>
         </form>
@@ -38,7 +60,7 @@ const mapStateToProps = ({users, questions, authedUser}) => ({
 })
 
 const mapDispatchToProps = {
-  
+  answerQuestion: (questionAnswer) => handleAnswerQuestion(questionAnswer)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Question)
